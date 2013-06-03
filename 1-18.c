b40c2bb7e9c4035e7647b879750bb365dd9c1e65
Craig Hagerman <craighagerman@gmail.com>
2013-06-03 23:09:54 +0900

    1-18 update

---
+++ 1-18.c
@@ -0,0 +1,43 @@
+/*
+   Exercise 1-18
+   Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines
+*/
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+
+int mygetline(char line[], int max);
+void chomp(char[] line);
+
+main()
+{
+   int len = 0;
+   char line[MAXLINE];
+   
+   while((len = mygetline(line, MAXLINE)) > 1 ) {
+      printf("%s \n", chomp(line));
+   }
+}
+
+
+int mygetline(char line[], int max) 
+{
+   int i = 0;
+   char c;
+   for (i; i<max && ((c=getchar()) != '\n' && c != EOF); i++) {
+      line[i] = c;   
+   }
+   line[i++] = '\0';
+   return i;
+}
+
+
+void chomp(char[] line) 
+{
+   
+   for (int i = 0; line[i] != '\0'; i++) {
+   
+   }
+   
+}
---
+++ 1-19.c
@@ -0,0 +1,5 @@
+/*
+   Exercise 1-19
+   Write a function reverse(s) that reverses the characters string s, Use it to write a program that reverses its input a line at a time. 
+*/
+
